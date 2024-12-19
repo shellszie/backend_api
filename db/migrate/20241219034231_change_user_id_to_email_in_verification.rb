15cd780d17dd4f0b20b55dd70b28f345eaed55bf
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2024-12-19 13:09:29 -0800

    removed unnecessary migrations

--- db/migrate/20241219034231_change_user_id_to_email_in_verification.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserIdToEmailInVerification < ActiveRecord::Migration[8.0]
-  def change
-    change_column :verifications, :user_id, :string
-  end
-end
--- db/migrate/20241219035010_change_user_email.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserEmail < ActiveRecord::Migration[8.0]
-  def change
-    rename_column :verifications, :user_id, :email
-  end
-end
